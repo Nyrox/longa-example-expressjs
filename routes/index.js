b4d59f696d272ea91b86b420ecdc662ef4cb7b5e
Nyrox <root@nyrox.tech>
2019-05-02 12:23:44 +0200

    Renames project from shippy to longa

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Shippy Example' });
+  res.render('index', { title: 'Express longa Example' });
 });
 
 module.exports = router;
